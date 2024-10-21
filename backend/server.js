42cbe6a30e5d01452a4641bc7391699c4dc1ff07
Mira Kaul <mirakaul@Miras-MacBook-Air-2.local>
2024-10-20 21:22:03 -0400

    Sprint 5 week 1: SortAlphaButton - Shayne and Mira

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+// Needed to add this for sprint 5 week 1 shayne and mira to work
+const cors = require('cors');
+app.use(cors()); // Enable CORS for all routes
 
 const { neon } = require("@neondatabase/serverless");
 
@@ -45,5 +48,18 @@ app.get('/query', async (req, res) => {
         console.error('Error querying the database:', error);
         res.status(500).send('Internal Server Error'); // Send an error response
       }
+});
+
+app.get('/sortalphaquery', async (req, res) => {
+  try {
+      // Query the 'dummy_data' table
+      const result = await sql`SELECT id, name FROM dummy_data WHERE id >= 500 AND id<= 599 ORDER BY name;`;
+      
+      // Send the result back to the client
+      res.json(result); // Send the result as a JSON response
+    } catch (error) {
+      console.error('Error querying the database:', error);
+      res.status(500).send('Internal Server Error'); // Send an error response
+    }
 });
 
