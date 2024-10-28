4b520abd29101c6d1dfe3f939f1eaf5c745b854c
William Goldman <60369048+iliketocode2@users.noreply.github.com>
2024-10-27 21:03:47 -0400

    started making component!

---
+++ frontend/src/app/components/ExpandedEntry.jsx
@@ -0,0 +1,20 @@
+
+//!IMPORTANT
+//Use the SelectItem button component created by Dan and Elisa, to select an item based on a particular id.
+//IMPORTANT
+import './ExpandedEntry.css';
+
+function ExpandedEntry() {
+    return (
+        <div className="background">
+            <div id="text">
+                <img id="image"
+                    src="https://www.hagca.com/uploads/1/2/7/1/127145683/holden-kittelberger-headshot_orig.jpg"
+                    alt="Holden Kittelberger"
+                />
+            </div>
+        </div>
+    );
+  }
+
+  export default ExpandedEntry;
