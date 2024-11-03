d2c5332afc67543a936e98b3ca7b57762e85bd80
zack-white <zack.white98@yahoo.com>
2024-11-03 14:41:50 -0500

    pool explanation

--- frontend/api/db.js
+++ frontend/api/db.js
@@ -3,6 +3,7 @@ import pg from 'pg';
 const { Pool } = pg;
 import 'dotenv/config';
 
+// We are using pooled connections because we are using serverless functions
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   max: 10, // maximum number of connections in the pool
