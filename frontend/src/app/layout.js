e8793d3496e69c37f1f4fc2406600a7c1a28e367
Elias Swartz <109711411+EliasThaKid@users.noreply.github.com>
2024-11-03 22:04:58 -0500

    Table and Unit set up plus early page integration

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -9,6 +9,7 @@ import SelectItemButton from "./components/SelectItem";
 import DeleteItemButton from "./components/DeleteItemButton";
 import ELiTable from "./components/Sprint15Tables/EliTable";
 import AddItemButton from "./components/AddItemButton";
+import ELiUnit from "./components/Sprint15Tables/EliUnit";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -32,6 +33,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <ELiUnit/>
         <ELiTable/>
       </body>
     </html>
