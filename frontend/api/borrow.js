298196b346fae7750e65652e4a0be9374ae61b83
Angie Zhang <angiezhang@Angies-MacBook-Pro.local>
2024-11-05 22:56:54 -0500

    button now filters out unavailable items when mass borrowing

--- frontend/api/borrow.js
+++ frontend/api/borrow.js
@@ -8,23 +8,49 @@ export default async function handler(req, res) {
   console.log(dateBorrowed, borrowerName, borrowerEmail, returnDate, selectedItems);
 
   try {
-    const statusNote = `Borrowed on Date: ${dateBorrowed}, Return Date: ${returnDate}`; 
-    const borrowHistoryNote = `Borrower Name: ${borrowerName}, Borrower Email: ${borrowerEmail}`;
+   
+    const borrowHistoryNote = `Borrower Name: ${borrowerName}, Borrower Email: ${borrowerEmail}, Borrowed on Date: ${dateBorrowed}, Return Date: ${returnDate}`;
 
-    console.log('Status Note:', statusNote); 
+    let availableItems = [];
+    let unavailableItems = []; 
+
+   
     console.log('History Note:', borrowHistoryNote);
 
     for (const itemId of selectedItems) {
       console.log(itemId);
+
+      const statusResult = await query(
+        'SELECT status FROM dummy_data WHERE id = $1', [itemId] 
+      ); 
+
+      const itemStatus = statusResult.rows[0]?.status; 
+      
+      if(itemStatus === 'Borrowed') {
+        console.log(`Item ${itemId} is already borrowed.`);
+        unavailableItems.push(itemId); 
+        continue; 
+      }
 
       // Using array_append with COALESCE to ensure borrower_history is an array
       await query(
         'UPDATE dummy_data SET status = $1, borrower_history = array_append(COALESCE(borrower_history, \'{}\'), $2) WHERE id = $3', 
         ['Borrowed', borrowHistoryNote, itemId]
       );
+
+      availableItems.push(itemId);
     }
 
-    res.status(200).json({ message: 'Items added successfully' });
+    let message = 'Borrowing process completed.';
+
+    if (availableItems.length > 0) {
+      message += `Sucessfully borrowed items ${availableItems.join(', ')}.`
+    }
+    if (unavailableItems.length > 0) {
+      message += `The following items were unavailable: ${unavailableItems.join(', ')}.`
+    }
+
+    res.status(200).json({ message });
 
   } catch (error) {
     console.error("Database query error:", error);
