dde19bc3af6010809e58a67550b4b56283b08d6e
shaynesidman <shaynesidman@Shaynes-MacBook-Pro.local>
2024-11-08 11:43:28 -0500

    almost inished Dashboard and filters components; ignore for now Zack

--- frontend/src/app/components/Checkbox.jsx
+++ frontend/src/app/components/Checkbox.jsx
@@ -5,15 +5,17 @@ import "./Checkbox.css";
 
 const Checkbox = (props) => { /* Props has element label with the label for dropdown name */
 
-    const [color, setColor] = useState("#ffffff");
+    const [color, setColor] = useState("#D9D9D9");
+    const [checked, setChecked] = useState("#D9D9D9")
 
     const handleClick = () => {
-        setColor(color === "#ffffff" ? "#8d0722" : "#ffffff");
+        setColor(color === "#D9D9D9" ? "#9B525F" : "#D9D9D9");
+        setChecked(color === "##D9D9D9" ? "#ffffff" : "#D9D9D9")
     }
 
     return (
         <div className="checkbox">
-            <button className="checkbox-button" onClick={handleClick} style={{backgroundColor: color}}> ✓ </button>
+            <button className="checkbox-button" onClick={handleClick} style={{backgroundColor: color, color: checked}}> ✓ </button>
             <p className="check-text">{props.label}</p>
         </div>
     );
