91a6cc496f6788177cb28b02401ea571c7fa4014
Peter Morganelli <peter.morganelli@tufts.edu>
2024-11-13 22:00:02 -0500

    made Tabs component for will and peter

---
+++ frontend/src/app/components/Tabs.jsx
@@ -0,0 +1,33 @@
+/*Tabs.jsx has 5 things:
+* 1. Logo (no action)
+    - PNG file
+* 2. Tab Button 1 (pie chart) --> bring to dashboard
+    -Dashboard component
+    - onClick --> bring to dashboard
+    - Components:
+        - Inventory Dashboard: *Need to implement* 
+        - Overview: Dashboard.jsx
+        - Recent Borrows: *Need to implement*
+* 3. Tab Button 2 (grid) --> brings to inventory grid component
+    - 
+* 4. Tab Button 3  (funnel) --> pop out funnel model
+    -Filters component
+* 5. Settings Button (settings icon)
+    -Use Feathericons.dev Settings logo (or the Figma one>)
+*/
+
+'use client';
+
+import React, { useState } from 'react';
+import Filter from './Filters';
+import Dashboard from "./components/Dashboard";
+import ELiTable from "./components/15Tablecomp/EliTable";
+
+
+const Tabs = () => {
+    <div>
+        
+    </div>
+};
+
+export default Tabs;
