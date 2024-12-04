d98416e1ae8ee33085242afc7e2fc8b860ac6cf3
shaynesidman <shaynesidman@Shaynes-MacBook-Pro.local>
2024-12-04 11:53:29 -0500

    Login page commit

---
+++ frontend/src/app/app.js
@@ -0,0 +1,59 @@
+"use client";
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './home';
+import Login from './login';
+import ForgotPassword from './forgot_password';
+import CreateAccount from './create_account';
+import Inventory from './inventory';
+import ConfirmSignup from './confirm_signup';
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [email, setEmail] = useState('')
+
+useEffect(() => {
+  // Fetch the user email and token from local storage
+  const user = JSON.parse(localStorage.getItem('user'))
+
+  // If the token/email does not exist, mark the user as logged out
+  if (!user || !user.token) {
+    setLoggedIn(false)
+    return
+  }
+
+  // If the token exists, verify it with the auth server to see if it is valid
+  fetch('http://localhost:3080/verify', {
+    method: 'POST',
+    headers: {
+      'jwt-token': user.token,
+    },
+  })
+    .then((r) => r.json())
+    .then((r) => {
+      setLoggedIn('success' === r.message)
+      setEmail(user.email || '')
+    })
+}, [])
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={<Home email={email} loggedIn={loggedIn} setLoggedIn={setLoggedIn} />}
+          />
+          <Route path="/login" element={<Login setLoggedIn={setLoggedIn} setEmail={setEmail} />} />
+          <Route path="/forgot_password" element={<ForgotPassword />} />
+          <Route path="/create_account" element={<CreateAccount />} />
+          <Route path="/inventory" element={<Inventory />} />
+          <Route path="/confirm_signup" element={<ConfirmSignup />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/app/forgot_password.js
@@ -0,0 +1,13 @@
+"use client";
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './login.css';
+
+export default function forgot_password () {
+    return (
+        <div className={'TheGoat'}>
+            <h1> if you forgot your password, you should try writing it down next time </h1>
+            <h1> womp womp </h1>
+        </div>
+    )
+}
