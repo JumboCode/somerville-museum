cf7ad20e2b89167cb189f6e8e226c0e70195e86e
Dan Glorioso <125602370+danglorioso@users.noreply.github.com>
2024-12-04 22:23:45 -0500

    Last commit on this branch? Creating a new one

--- frontend/api/selectCounts.js
+++ frontend/api/selectCounts.js
@@ -3,6 +3,7 @@ import { query } from './db.js';
 export default async function handler(req, res) {
     const { name } = req.body;
     try {
+      console.log("In the selectCounts API");
       const result = await query('SELECT COUNT(*) FROM dummy_data WHERE status = $1', [name]);
       const count = result.rows[0].count;  // Extract the count from the result
       res.status(200).json({ count });  // Send it as part of an object
