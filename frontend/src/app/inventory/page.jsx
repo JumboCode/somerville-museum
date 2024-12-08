b3ff831af5ff3f7b8e3b5379bf2b99fb6227995c
zack-white <zack.white98@yahoo.com>
2024-12-08 15:57:50 -0500

    we are so back

--- frontend/src/app/inventory/page.jsx
+++ frontend/src/app/inventory/page.jsx
@@ -8,80 +8,83 @@ import AddPopup from '../components/AddItemButton';
 import ReturnButton from '../components/ReturnButton';
 
 export default function Inventory() {
-  const [units, setUnits] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [unitsPerPage, setUnitsPerPage] = useState(10);
-  const [totalPages, setTotalPages] = useState();
-  const [selectedItems, setSelectedItems] = useState([]);
+    const [units, setUnits] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [unitsPerPage, setUnitsPerPage] = useState(10);
+    const [totalPages, setTotalPages] = useState();
+    const [selectedItems, setSelectedItems] = useState([]);
 
-  useEffect(() => {
-    console.log(selectedItems);
-    fetchData();
-  }, [selectedItems]);
+    useEffect(() => {
+        console.log(selectedItems);
+        fetchData();
+    }, [selectedItems]);
 
-  async function fetchData() {
-    try {
-      const response = await fetch(`../../api/queryAll`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-      });
-      
-      if (response.ok) {
-        const data = await response.json();
-        const currentDate = new Date();
-        const updatedData = data.map((item) => {
-          if (item.status === 'Borrowed' && item.dueDate && new Date(item.dueDate) < currentDate) {
-            return { ...item, status: 'Overdue' };
-          }
-          return item;
-        });
-        
-        setUnits(updatedData);
-        setTotalPages(Math.ceil(updatedData.length / unitsPerPage));
-      } else {
-        console.error("failed to fetch data");
-      }
-    } catch (error) {
-      console.error(error);
+    async function fetchData() {
+        try {
+            const response = await fetch(`../../api/queryAll`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                const currentDate = new Date();
+                const updatedData = data.map((item) => {
+                    if (item.status === 'Borrowed' && item.dueDate && new Date(item.dueDate) < currentDate) {
+                        return { ...item, status: 'Overdue' };
+                    }
+                    return item;
+                });
+
+                setUnits(updatedData);
+                setTotalPages(Math.ceil(updatedData.length / unitsPerPage));
+            } else {
+                console.error("failed to fetch data");
+            }
+        } catch (error) {
+            console.error(error);
+        }
     }
-  }
 
-  const handleBorrowSuccess = () => {
-    fetchData();
-  };
+    const handleBorrowSuccess = () => {
+        fetchData();
+    };
 
-  const handleCheckboxChange = (unit, isChecked) => {
-    console.log("HYING HERE RAHHH", unit);
-    setSelectedItems((prevSelected) => {
-      if (isChecked) {
-        console.log("MKASDMDKADMF");
-        return [unit, ...prevSelected];
-      } else {
-        console.log("nettspend")
-        return prevSelected.filter(item => item.id !== unit.id);
-      }
-    });
-  };
+    const handleCheckboxChange = (unit, isChecked) => {
+        console.log("HYING HERE RAHHH", isChecked, unit);
+        setSelectedItems((prevSelected) => {
+            if (isChecked) {
+                console.log([unit, ...prevSelected]);
+                return [unit, ...prevSelected];
+            } else {
+                console.log("nettspend")
+                return prevSelected.filter(item => item.id !== unit.id);
+            }
+        });
+    };
 
-  const startIndex = (currentPage - 1) * unitsPerPage;
-  const currentUnits = units
-    .slice(startIndex, startIndex + unitsPerPage)
-    .map((unit) => (
-      <InventoryUnit
-        key={unit.id}
-        unit={unit}
-        onChange={handleCheckboxChange}
-        // checked={selectedItems.some((item) => item?.id && unit?.id && item.id === unit.id)}
-        checked={false}
-      />
-    ));
+    const startIndex = (currentPage - 1) * unitsPerPage;
+    const currentUnits = units
+        .slice(startIndex, startIndex + unitsPerPage)
+        .map((unit) => {
+            console.log(selectedItems);
+            return (<InventoryUnit
+                key={unit.id}
+                unit={unit}
+                onChange={handleCheckboxChange}
+                checked={selectedItems.some((item) => item?.id && unit?.id && item.id === unit.id)}
+            // checked={true}
+            />)
+        }
+
+        );
 
     const sortByName = () => {
         const filteredAndSortedEntries = [...units]
-          .sort((a, b) => a.name.localeCompare(b.name)); // Sort alphabetically
-    
+            .sort((a, b) => a.name.localeCompare(b.name)); // Sort alphabetically
+
         setUnits(filteredAndSortedEntries);
 
     };
@@ -89,7 +92,6 @@ export default function Inventory() {
 
     //tesing piece of code
     // const totalPages = Math.ceil(20 / unitsPerPage);
-
     const goToPreviousPage = () => {
         setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
     };
@@ -102,36 +104,34 @@ export default function Inventory() {
     // set units to the selected value
     const handleUnitsPerPageChange = (event) => {
         setUnitsPerPage(Number(event.target.value));
-        setCurrentPage(1); 
-        setTotalPages(Math.ceil(units.length/ Number(event.target.value)));//by default set the current page to 1
+        setCurrentPage(1);
+        setTotalPages(Math.ceil(units.length / Number(event.target.value)));//by default set the current page to 1
     };
 
     // an array of buttons for page selection
-    const buttons = Array.from({ length: totalPages}, (_, index) => index + 1);
+    const buttons = Array.from({ length: totalPages }, (_, index) => index + 1);
 
     return (
-            <>
-        
+        <>
+
             <div className="Table">
                 <div className="Header">
                     <div className="Items">
                         <div className="Searchbar">
-                            <input type="text" placeholder="Search..."/>
+                            <input type="text" placeholder="Search..." />
                         </div>
-                            <div className='buttons'> 
-                                <AddPopup className='addBtn'>+item</AddPopup>
-                                <BorrowButton className='brwBtn' 
+                        <div className='buttons'>
+                            <AddPopup className='addBtn'>+item</AddPopup>
+                            <BorrowButton className='brwBtn'
                                 selectedItems={selectedItems}
-                                onSuccess = {handleBorrowSuccess}>Borrow</BorrowButton>
-                                selectedItems = {selectedItems} 
-                                onSuccess = {handleBorrowSuccess}
-                                <ReturnButton className='brwBtn' //added return button here
-                                selectedItems = {selectedItems}
+                                onSuccess={handleBorrowSuccess}>Borrow
+                            </BorrowButton>
+                            <ReturnButton className='brwBtn' //added return button here
+                                selectedItems={selectedItems}
                                 onSuccess={handleBorrowSuccess}
-                                >RETURN</ReturnButton>
+                            >RETURN</ReturnButton>
+                        </div>
 
-                            </div>
-                        
                     </div>
                     <div className="TableLabels">
                         <div className="TableLabel"> ID </div>
@@ -146,7 +146,7 @@ export default function Inventory() {
                 <div className="pagination-controls">
                     <div className="num-items">
                         <p className="view">View </p>
-                        <select className="select-num" id="select-num" onChange = {handleUnitsPerPageChange}>
+                        <select className="select-num" id="select-num" onChange={handleUnitsPerPageChange}>
                             <option value="10">10</option>
                             <option value="25">25</option>
                             <option value="50">50</option>
@@ -157,12 +157,12 @@ export default function Inventory() {
                         <button className="leftBtn" onClick={goToPreviousPage} disabled={currentPage === 1}>
                             &lt;
                         </button>
-                            {buttons.map((number) => (
-                                <button className="pageNum" key={number} onClick={() => setCurrentPage(number)}>
-                                    {number}
-                                </button>
-                            ))}
-                        
+                        {buttons.map((number) => (
+                            <button className="pageNum" key={number} onClick={() => setCurrentPage(number)}>
+                                {number}
+                            </button>
+                        ))}
+
                         <button className="rightBtn" onClick={goToNextPage} disabled={currentPage === totalPages}>
                             &gt;
                         </button>
@@ -170,6 +170,6 @@ export default function Inventory() {
                 </div>
             </div>
         </>
-        
+
     );
 }
