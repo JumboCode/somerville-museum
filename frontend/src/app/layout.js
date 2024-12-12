e2d7e54efcd6f676e5a25d0279bf0f42cdeea944
zack-white <zack.white98@yahoo.com>
2024-12-12 13:06:33 -0500

    favicon

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -5,6 +5,14 @@ import Tabs from './components/Tabs';
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
+            <head>
+                <title>Database</title>
+                <meta name="description" content="Database for the Somerville Museum" />
+                {/* favicon svg */}
+                <link rel="icon" type="image/svg+xml" href="/sm-logo.svg" />
+                {/* favicon png as backup */}
+                <link rel="icon" type="image/png" sizes="32x32" href="/sm-logo.png" />
+            </head>
             <body>
                 <div className="app-layout">
                     <Tabs />
