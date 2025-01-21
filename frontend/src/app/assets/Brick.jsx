09d3f3d9aa99ca92f05214d8d94c7d580e44a99b
William Goldman <60369048+iliketocode2@users.noreply.github.com>
2025-01-20 21:06:34 -0500

    started fixing nav bar

--- frontend/src/app/assets/Brick.jsx
+++ frontend/src/app/assets/Brick.jsx
@@ -7,15 +7,10 @@ const Brick = () => {
     return (
         <Link href="/inventory">
             <svg
-                id="brick"
                 xmlns="http://www.w3.org/2000/svg"
-                width="2rem"
-                height="2rem"
                 viewBox="0 0 16 16"
-                fill="none"
-                style={{ cursor: 'pointer' }}
             >
-                <path d="M7 1H1V5H7V1Z M7 7H1V15H7V7Z M9 1H15V9H9V1Z M15 11H9V15H15V11Z" fill="#000000" />
+                <path d="M7 1H1V5H7V1Z M7 7H1V15H7V7Z M9 1H15V9H9V1Z M15 11H9V15H15V11Z" />
             </svg>
         </Link>
     );
