adaef7c3de5a8781419b6884e033eb61f93137f7
Dan Glorioso <125602370+danglorioso@users.noreply.github.com>
2025-01-25 16:31:29 -0500

    Working on new addPage component, added through file upload
    Co-authored-by: Masmooo <Masmooo@users.noreply.github.com>

---
+++ frontend/src/app/add/page.jsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import AddPage from "../components/AddPage";
+import "../components/AddPage.css"
+
+
+const Add = () => {
+    return (
+        <div className="add-page">
+        <AddPage />
+        </div>
+    );
+};
+
+export default Add;
