bf5925b39028742a1845075d403eb3dbd70f0f42
Dan Glorioso <125602370+danglorioso@users.noreply.github.com>
2025-01-29 23:38:55 -0500

    Work from Wednesday night

--- frontend/src/app/components/AddPage.jsx
+++ frontend/src/app/components/AddPage.jsx
@@ -16,21 +16,60 @@ export default function AddPage() {
     const [note, setNote] = useState('');
     const [selectedGarment, setSelectedGarment] = useState("");
     const [selectedTimePeriod, setSelectedTimePeriod] = useState("");
-    const [age, setAge] = useState('');
-    const [gender, setGender] = useState([]);
     const [selectedSize, setSelectedSize] = useState([]);
     const [Season, setSeason] = useState([]);
     const [condition, setCondition] = useState([]);
     const [color, setColor] = useState([]);
     const [text, setText] = useState("");
 
-    const [youthSelection, setYouthSelection] = useState(null);
+    const [ageSelection, setAgeSelection] = useState(null);
     const [adultSelection, setAdultSelection] = useState(null);
     const [genderSelection, setGenderSelection] = useState(null);
+    const [selectedSeason, setSelectedSeason] = useState(null);
+    const [selectedColors, setSelectedColors] = useState([]);
+    const [selectedChoice, setSelectedChoice] = useState([]);
 
-    const options = ["Youth", "Adult"];
+    const ageOptions = ["Youth", "Adult"];
     const genderOptions = ["Male", "Female", "Unisex"];
+    const seasons = ["Fall", "Winter", "Spring", "Summer"];
+    const conditions = ["Needs repair", "Needs dry cleaning", "Needs washing", "Not usable", "Great"];
+    const garments = [
+        { label: "Gowns/dresses", value: "Gowns/dresses" },
+        { label: "Outerwear", value: "Outerwear" },
+        { label: "Accessories", value: "Accessories" },
+        { label: "Bottoms", value: "Bottoms" },
+        { label: "Shoes", value: "Shoes" },
+        { label: "Socks/hose", value: "Socks/hose" },
+        { label: "Tops", value: "Tops" },
+        { label: "Vests", value: "Vests" }
+    ];
+    const timePeriods = [
+        { name: "Post-1920s" },
+        { name: "Pre-1700s" },
+        { name: "1750s-1800s" },
+        { name: "1800s-1840s" }
+    ];
+    const sizes = [
+        { label: "Small", value: "small" },
+        { label: "Medium", value: "medium" },
+        { label: "Large", value: "large" },
+        { label: "X-Large", value: "x-large" }
+    ];
 
+    const cancelOrSubmit = ["Cancel", "Submit"];
+    const colors = [
+        { name: "Red", hex: "#FF3B30" },
+        { name: "Orange", hex: "#FF9500" },
+        { name: "Yellow", hex: "#FFCC00" },
+        { name: "Green", hex: "#34C759" },
+        { name: "Blue", hex: "#5856D6" },
+        { name: "Purple", hex: "#AF52DE" },
+        { name: "Pink", hex: "#FF2D55" },
+        { name: "Brown", hex: "#A2845E" },
+        { name: "White", hex: "#FFFFFF", border: "#c9c9c9" },
+        { name: "Gray", hex: "#8E8E93" },
+        { name: "Black", hex: "#000000" },
+      ];
 
     const handleGarmentSelection = (value) => {
         setGarmentTag(value);
@@ -66,30 +105,13 @@ export default function AddPage() {
         setText(event.target.value);
     };
 
-    const garments = [
-        { label: "Gowns/dresses", value: "Gowns/dresses" },
-        { label: "Outerwear", value: "Outerwear" },
-        { label: "Accessories", value: "Accessories" },
-        { label: "Bottoms", value: "Bottoms" },
-        { label: "Shoes", value: "Shoes" },
-        { label: "Socks/hose", value: "Socks/hose" },
-        { label: "Tops", value: "Tops" },
-        { label: "Vests", value: "Vests" }
-    ];
-
-    const timePeriods = [
-        { name: "Post-1920s" },
-        { name: "Pre-1700s" },
-        { name: "1750s-1800s" },
-        { name: "1800s-1840s" }
-    ];
-
-    const sizes = [
-        { label: "Small", value: "small" },
-        { label: "Medium", value: "medium" },
-        { label: "Large", value: "large" },
-        { label: "X-Large", value: "x-large" }
-    ];
+    const handleSelect = (color) => {
+        if (selectedColors.includes(color)) {
+          setSelectedColors(selectedColors.filter((c) => c !== color));
+        } else if (selectedColors.length < 2) {
+          setSelectedColors([...selectedColors, color]);
+        }
+      };
 
     return (
         <div className="main">
@@ -220,47 +242,107 @@ export default function AddPage() {
                         </div>
                     </div>
                     
-                    <div className="ageGenderRow">
+                    <div className="age-and-gender">
+
+                        {/* Age Buttons */}
                         <div className="allAge">
-                            <div className="ageName">
-                                Age Group*
-                            </div>
+                            <h3>Age Group*</h3>
                             <div className="ageRow">
-                                <div className="youthButton">
+                                <div className="ageButton">
                                     <SelectButton 
-                                        value={youthSelection} 
-                                        options={options} 
-                                        onChange={(e) => setYouthSelection(e.value)} 
-                                        ariaLabel="Youth Selection" 
+                                        value={ageSelection} 
+                                        options={ageOptions}
+                                        onChange={(e) => setAgeSelection(e.value)} 
+                                        ariaLabel="Age Selection" 
+                                        classname="ageButton"
                                     />
                                 </div>
                             </div>
                         </div>
+
+                        {/* Gender Buttons */}
                         <div className="allGender">
-                            <div className="genderName">
-                                Gender*
-                            </div>
+                            <h3>Gender*</h3>
                             <div className="genderButtons">
                                     <SelectButton 
                                         value={genderSelection} 
                                         options={genderOptions}
                                         onChange={(e) => setGenderSelection(e.value)} 
-                                        ariaLabel="Youth Selection" 
+                                        ariaLabel="Gender Selection" 
                                     />
                             </div>
                         </div>
                     </div>
 
-
                     {/* Size Buttons */}
                     <div className="size-buttons">
                         <h3>Size*</h3>
-
-                        <div className="button">
-                            <SelectButton value={selectedSize} onChange={(e) => setSelectedSize(e.value)} options={sizes} />
-                        </div>      
+                        <SelectButton 
+                            value={selectedSize} 
+                            onChange={(e) => setSelectedSize(e.value)} 
+                            options={sizes} 
+                        />
                     </div>
 
+
+                    {/* Size Buttons */}
+                    <div className="size-buttons">
+                        <h3>Season*<span style={{fontWeight: "400"}}> (Max of 2)</span></h3> 
+                        <SelectButton value={selectedSeason} onChange={(e) => setSelectedSeason(e.value)} options={seasons} />
+                    </div>
+
+                    {/* Condition Dropdown */}
+                    <div className="condition-component">
+                        <div className="condition-dropdown">
+                            <h3>Condition*<span style={{fontWeight: "400"}}> (Max of 2)</span></h3> 
+                            <Dropdown
+                                value={condition}
+                                options={conditions}
+                                onChange={(e) => setCondition(e.value)}
+                                placeholder="Select Condition"
+                                className="dropdown"
+                            />
+                        </div>
+                    </div>
+                            
+                    {/* Color Selector */}
+                    <div className="color-component">
+                        <div className="color-dropdown">
+                            <h3>Color*<span style={{fontWeight: "400"}}> (Max of 2)</span></h3> 
+                            <div className="color-selector">
+                                <div className="color-options">
+                                    {colors.map((color) => (
+                                    <div
+                                        key={color.name}
+                                        className={`color-circle ${selectedColors.includes(color.name) ? "selected" : ""}`}
+                                        style={{
+                                        backgroundColor: color.hex,
+                                        border: color.border ? `2px solid ${color.border}` : "none",
+                                        }}
+                                        onClick={() => handleSelect(color.name)}
+                                    ></div>
+                                    ))}
+                                </div>
+                                <p className="selected-text">
+                                    Selected: {selectedColors.length > 0 ? selectedColors.join(", ") : "None"}
+                                </p>
+                                </div>
+                            
+                        </div>
+                    </div>
+
+                        <div className="cancel-submit">
+                            <div className="cancel-submit-buttons">
+                                <div className="ageButton">
+                                    <SelectButton 
+                                        value={selectedChoice} 
+                                        onChange={(e) => setSelectedChoice(e.value)} 
+                                        options={cancelOrSubmit}
+                                        
+                                    />
+                                </div>
+                            </div>           
+                        </div>
                 </div>
             </div>    
         </div>
