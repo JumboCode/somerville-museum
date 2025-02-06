73cfcfbea84ae8aab9d2ff630075ec4866b0e136
hjiang06 <138931704+hjiang06@users.noreply.github.com>
2025-02-06 17:08:05 -0500

    lowk cooked on the css <3

--- frontend/src/app/components/DeleteItemButton.js
+++ frontend/src/app/components/DeleteItemButton.js
@@ -2,69 +2,82 @@
 
  import { useState } from "react";
 import StylishButton from "./StylishButton";
+import DeletePopup from "./DeletePopup";
 
  export default function DeleteItemButton() {
-     const handleClick = () => {
+    const [isPopupVisible, setPopupVisible] = useState(false);
+    const handleClick = () => {
 
-         // Replace the alert with your actual functionality.
-         let name = prompt("Please enter item name", "");
+        setPopupVisible(true)
+        //  // Replace the alert with your actual functionality.
+        //  let name = prompt("Please enter item name", "");
 
-         if (name == null || name == "") {
-             alert("User cancelled the prompt.");
-         } else {
-                 // Call the 'select' endpoint to get the item ID
-                 fetch(`../../api/selectByName`, { 
-                    method: 'PUT',
-                    headers: {
-                    'Content-Type': 'application/json' // Specify the content type
-                    },
-                    body: JSON.stringify({ name }) // Send the name as a JSON object
-                  })
-                     .then(response => response.json()) // Convert the response to JSON
+        //  if (name == null || name == "") {
+        //      alert("User cancelled the prompt.");
+        //  } else {
+        //          // Call the 'select' endpoint to get the item ID
+        //          fetch(`../../api/selectByName`, { 
+        //             method: 'PUT',
+        //             headers: {
+        //             'Content-Type': 'application/json' // Specify the content type
+        //             },
+        //             body: JSON.stringify({ name }) // Send the name as a JSON object
+        //           })
+        //              .then(response => response.json()) // Convert the response to JSON
 
-                     // Send the ID to the delete route
-                     .then(data => {
-                         if (data.length > 0) {
-                             // Map the data to get the item IDs
-                             const itemIds = data.map(item => item.id);
+        //              // Send the ID to the delete route
+        //              .then(data => {
+        //                  if (data.length > 0) {
+        //                      // Map the data to get the item IDs
+        //                      const itemIds = data.map(item => item.id);
 
-                             //Comfirm with the user if they really want to delete the item
-                             if (confirm("Do you really want to delete the selected item(s)? \nSelected Item(s): " + itemIds)) {
-                                 // For each item ID, make a GET request to the 'delete' endpoint
-                                 itemIds.forEach(id => {
-                                    fetch(`../../api/db`, { 
-                                        method: 'PUT', // Use the PUT method
-                                        headers: {
-                                          'Content-Type': 'application/json' // Specify the content type
-                                        },
-                                        body: JSON.stringify({
-                                          text: 'DELETE FROM dummy_data WHERE id = $1', // SQL query for deletion
-                                          params: [id] // Parameters for the query
-                                        })
-                                      });
-                                 });
-                                } else {
-                                    alert('User cancelled the deletion.');
-                                }
-                            } else {
-                                alert("Item(s) not found.");
-                            }
-                        })
+        //                      //Comfirm with the user if they really want to delete the item
+        //                      if (confirm("Do you really want to delete the selected item(s)? \nSelected Item(s): " + itemIds)) {
+        //                          // For each item ID, make a GET request to the 'delete' endpoint
+        //                          itemIds.forEach(id => {
+        //                             fetch(`../../api/db`, { 
+        //                                 method: 'PUT', // Use the PUT method
+        //                                 headers: {
+        //                                   'Content-Type': 'application/json' // Specify the content type
+        //                                 },
+        //                                 body: JSON.stringify({
+        //                                   text: 'DELETE FROM dummy_data WHERE id = $1', // SQL query for deletion
+        //                                   params: [id] // Parameters for the query
+        //                                 })
+        //                               });
+        //                          });
+        //                         } else {
+        //                             alert('User cancelled the deletion.');
+        //                         }
+        //                     } else {
+        //                         alert("Item(s) not found.");
+        //                     }
+        //                 })
    
-                        .catch(error => {
-                            console.error('Error fetching item:', error);
-                            alert('Error fetching item. Please try again later.');
-                        });
-            }
+        //                 .catch(error => {
+        //                     console.error('Error fetching item:', error);
+        //                     alert('Error fetching item. Please try again later.');
+        //                 });
+        //     }
+        };
+
+        const handleCancel = () => {
+            setPopupVisible(false);
         };
    
         return (
-            <StylishButton
-                className="text-center border p-2 rounded bg-gray-100 hover:bg-gray-200"
-                onClick={handleClick}
-                label = "Delete"
-            >
+            <>
+                <StylishButton
+                    className="text-center border p-2 rounded bg-gray-100 hover:bg-gray-200"
+                    onClick={handleClick}
+                    label = "Delete"
+                >
+                </StylishButton>
 
-            </StylishButton>
+                {isPopupVisible && (
+                <DeletePopup onConfirm={() => console.log("balls")} onCancel={handleCancel} />
+            )}
+
+            </>
         );
     }
