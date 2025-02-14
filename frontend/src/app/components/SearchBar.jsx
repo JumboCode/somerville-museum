ddf62cccc2351179a996444aa21ac4deff1442fc
shaynesidman <147111519+shaynesidman@users.noreply.github.com>
2025-02-14 18:33:18 -0500

    Search bar almost done

--- frontend/src/app/components/SearchBar.jsx
+++ frontend/src/app/components/SearchBar.jsx
@@ -9,34 +9,29 @@ export default function SearchBar({ updateSearchResults }) {
     const enterQuery = (e) => {
         setQuery(e.target.value);
     };
-    // TODO: Implement backspace detection & test capitalization & test filters
+    // TODO: integrate w/ filters
     useEffect(() => {  // Fetch relevant search results when search query is changed
-        if (query) {
-            const fetchData = async () => {
-                try {
-                    const response = await fetch(`../../api/search`, {
-                        method: "PUT",
-                        headers: {
-                            "Content-Type": "application/json",
-                        },
-                        body: JSON.stringify({ searchQuery: query }),
-                    });
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`../../api/search`, {
+                    method: "PUT",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({ searchQuery: query }),
+                });
 
-                    if (!response.ok) {
-                        throw new Error("Network response was not ok");
-                    }
-
-                    const data = await response.json();
-                    console.log(data);
-                    updateSearchResults(data);  // Update results on inventory page
-                } catch (error) {
-                    console.log(error);
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
                 }
-            };
-            fetchData();
-        } else {
 
-        }
+                const data = await response.json();
+                updateSearchResults(data);  // Update results on inventory page
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        fetchData();
     }, [query]);
 
     return (
@@ -46,6 +41,7 @@ export default function SearchBar({ updateSearchResults }) {
                 placeholder="Search..."
                 value={query}
                 onChange={enterQuery}
+                onKeyDown={(e) => {if (e.key == 'Backspace') {enterQuery}}}  // Added for redundancy in detecting backspace; should work w/out this
             />
         </div>
     );
