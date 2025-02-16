9b1c04d8854ab43ad81dd11722b8bbb5509ab2e2
Dan Glorioso <125602370+danglorioso@users.noreply.github.com>
2025-02-16 00:52:34 -0500

    Added fun animations :O

--- frontend/src/app/components/DeletePopup.jsx
+++ frontend/src/app/components/DeletePopup.jsx
@@ -14,15 +14,31 @@
 
 import React from "react";
 import "./DeletePopup.css";
+import { useState } from "react";
 
 const DeletePopup = ({ onConfirm, onCancel, selectedItems }) => {
-    
+    const [isClosing, setIsClosing] = useState(false);
+
+    const handleCancel = () => {
+        setIsClosing(true);
+        setTimeout(() => {
+            onCancel();
+        }, 200);
+    };
+
+    const handleDelete = () => {
+        setIsClosing(true);
+        setTimeout(() => {
+            onConfirm();
+        }, 200);
+    };
+
     // Create list of ID, separated by commas
     const itemDetails = selectedItems.map(item => `${item.id}: ${item.name}`).join(", ");
     
     return (
-      <div className="deleteContainer">
-            <div className="deleteContent">
+      <div className={`deleteContainer ${isClosing ? 'fade-out' : 'fade-in'}`}>
+            <div className={`deleteContent`}>
                 <img src="/icons/important.svg" className="importantIcon"/>
                 <h2>Delete Item(s)</h2>
                 <p>Are you sure you want to delete the following item(s)?</p>
@@ -34,8 +50,8 @@ const DeletePopup = ({ onConfirm, onCancel, selectedItems }) => {
                 <p>You can't undo this action.</p>
 
                 <div className="buttons">
-                    <button onClick={onCancel} className="cancel-button confirmCancel">Cancel</button>
-                    <button onClick={onConfirm} className="confirm-button confirmDelete">Delete</button>
+                    <button onClick={handleCancel} className="cancel-button confirmCancel">Cancel</button>
+                    <button onClick={handleDelete} className="confirm-button confirmDelete">Delete</button>
                 </div>
             </div>
         </div>
