24cc5dcea83df927803a62b13c901f7692e8ae59
zack-white <zack.white98@yahoo.com>
2025-02-17 14:26:01 -0500

    env

--- frontend/api/sendReturnEmail.js
+++ frontend/api/sendReturnEmail.js
@@ -1,8 +1,6 @@
 import Mailjet from "node-mailjet";
 import { query } from './db.js';
 
-const MJ_APIKEY_PUBLIC='00abc5ea2cb2dc82c1d4613a4794823f'
-const MJ_APIKEY_PRIVATE='b74f4b1d4834f8ceee7da28ad504e96d'
 
 const mailjet = Mailjet.apiConnect(process.env.MJ_APIKEY_PUBLIC, process.env.MJ_APIKEY_PRIVATE);
 
