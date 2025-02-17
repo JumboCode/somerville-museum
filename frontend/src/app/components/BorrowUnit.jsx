7f77c9bf4a2b4e296c4554b8fb87d7a6710ce64a
Peter Morganelli <146687222+pmorganelli@users.noreply.github.com>
2025-02-17 01:15:34 -0500

    Update BorrowUnit.jsx

--- frontend/src/app/components/BorrowUnit.jsx
+++ frontend/src/app/components/BorrowUnit.jsx
@@ -1,33 +1,52 @@
+// BorrowUnit.jsx
+//
+// Edited by Peter Morganelli, 2/16/25
+//
+// Purpose: 
+//     This file handles the BorrowUnit functionality and uses some styling,
+//     which helps with the BorrowPopup component to organize the different selected items
 'use client'
 
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import "./BorrowPopup.css";
 
-const BorrowUnit = ({item, onDelete}) => {
-    if (!item) {
-        return null;
-    }
-    const {id, name} = item; 
-    //image, name, ID, X to delete from display 
-    //onChange={handleRemoveItem}
+const BorrowUnit = ({ item, onDelete }) => {
+  if (!item) return null;
+  const { id, name } = item;
+  //truncate the name to at most 12 characters with additional ellipsis if needed
+  const truncatedName = name.length > 12 ? name.substring(0, 12) + "..." : name;
 
-    return ( 
-        <div className="borrow-unit">
-            <div className="borrow-unit-content">
-                <span className='item-id'>{id}</span>
-                <span className='item-name'>{name}</span>
-            </div>
-            <button
-                className='delete-button'
-                onClick={() => onDelete(item)}
-                aria-label={`Delete ${name}`}
-            >
-                X
-            </button>
-           
-        </div>
-        
-    )
+  return (
+    <div className="borrowed-item-box">
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT29tUf96oPGTyLihMp4z4rM2kgSzX4rXeTow&s"
+        alt="image placeholder"
+        className="item-image"
+      />
+      <span className="item-name">{truncatedName}</span>
+      <span className="item-id">ID #{id}</span>
+      <button
+        className="delete-button"
+        onClick={() => onDelete(item)}
+        aria-label={`Delete ${name}`}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          width="24"
+          height="24"
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+        >
+          <line x1="18" x2="6" y1="6" y2="18" />
+          <line x1="6" x2="18" y1="6" y2="18" />
+        </svg>
+      </button>
+    </div>
+  );
+};
 
-}
-
-export default BorrowUnit; 
+export default BorrowUnit;
