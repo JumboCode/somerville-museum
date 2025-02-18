4cdf42b7cc1219498a13fc61808b40431d91677b
hjiang06 <138931704+hjiang06@users.noreply.github.com>
2025-02-18 11:31:09 -0500

    set the pages up? or something like that... prolly not right

---
+++ frontend/src/app/borrowers/page.jsx
@@ -0,0 +1,11 @@
+"use client";
+import './BorrowerPage.css';
+import BorrowerPageUnit from './BorrowerPageUnit.jsx';
+import { useState, useEffect } from "react";
+import StylishButton from '../components/StylishButton.jsx';
+import SearchBar from '../components/SearchBar';
+
+export default function BorrowerPage(){
+
+}
+
