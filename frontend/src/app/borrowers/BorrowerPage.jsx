d85fddf3967d0b77a75eb360deb0da0a3fac91c3
hjiang06 <138931704+hjiang06@users.noreply.github.com>
2025-02-18 20:53:49 -0500

    idk wtf i'm doing

--- frontend/src/app/borrowers/BorrowerPage.jsx
+++ frontend/src/app/borrowers/BorrowerPage.jsx
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from 'react';
 import "../globals.css";
+import "../BorrowerPage.css"
 import { Dropdown } from 'primereact/dropdown';
 import { MultiSelect } from 'primereact/multiselect';
 import StylishButton from '../components/StylishButton.jsx';
