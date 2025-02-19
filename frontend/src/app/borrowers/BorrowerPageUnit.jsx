53123751c3c9cf51e249d63b2f79283b522dad1f
hjiang06 <138931704+hjiang06@users.noreply.github.com>
2025-02-18 20:46:20 -0500

    rip units, hello table

--- frontend/src/app/borrowers/BorrowerPageUnit.jsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { useState, useEffect, useRef, forwardRef} from "react";
-import Popup from "./Popup";
-import PrePopup from "./PrePopup";
-import "./BorrowerPage.css";
-
-export default function BorrowerPageUnit({ unit, onChange, checked }) {
-
-}
