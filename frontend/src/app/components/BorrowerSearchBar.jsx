e7f4fbb52e7fe7563c665fbad1595a6641a6bdbb
Peter Morganelli <petercarlmorganelli@gmail.com>
2025-02-28 22:16:00 -0500

    finished search functionality for borrowers table

---
+++ frontend/src/app/components/BorrowerSearchBar.jsx
@@ -0,0 +1,68 @@
+/**
+ * @fileoverview This file will implement the functionality for the BorrowerSearchBar,
+ *               utilizing similar functionality to the inventory SearchBar
+ *               
+ *               NOTES: Uses a POST request on the db.js endpoint which requires
+ *                      text and params as arguments, so I turned it into a JSON
+ *                      object and passed the stringified object into query
+ * 
+ * @file BorrowerSearchBar.jsx
+ * @date February 28th, 2025
+ * @authors Peter Morganelli
+ *  
+ */
+
+"use client";
+
+import "./SearchBar.css";
+import { useState, useEffect } from "react"; 
+
+export default function BorrowerSearchBar({ updateSearchResults }) {
+    const [query, setQuery] = useState("");
+
+    // Fetch relevant search results when search query is changed
+    useEffect(() => { 
+        console.log(query)
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`../../../../api/db`, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    //use parameterized query to prevent sql injection :p
+                    body: JSON.stringify({
+                        text: 'SELECT * FROM borrowers WHERE name ILIKE $1',
+                        params: [`%${query}%`],
+                      }),
+                });
+
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+
+                const data = await response.json();
+                updateSearchResults(data);  // Update results on inventory page
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        fetchData();
+    }, [query]);
+    return (
+        <div className="Searchbar">
+            <input 
+                type="text" 
+                placeholder="Search..."
+                value={query}
+                onChange={(e) => { setQuery(e.target.value); }}
+                onKeyDown={(e) => {
+                    if (e.key === "Backspace" || e.key === "Delete") {
+                        setQuery(e.target.value);
+                    }
+                }}
+            />
+
+        </div>
+    );
+}
