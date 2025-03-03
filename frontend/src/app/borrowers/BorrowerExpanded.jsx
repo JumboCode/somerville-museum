a438d85f25db3f4b6a897099d8e78bcbc6290817
hjiang06 <138931704+hjiang06@users.noreply.github.com>
2025-03-03 16:46:41 -0500

    asfd

--- frontend/src/app/borrowers/BorrowerExpanded.jsx
+++ frontend/src/app/borrowers/BorrowerExpanded.jsx
@@ -1,3 +1,14 @@
+ /**************************************************************
+ *
+ *                     BorrowerExpanded.jsx
+ *
+ *        Authors: Hannah Jiang & Peter Morganelli & Zack White
+ *           Date: 03/05/2025
+ *
+ *     Summary: The sidebar popup for a selected item in the borrower page.
+ * 
+ **************************************************************/
+
 "use client";
 import "./BorrowerExpanded.css";
 import StylishButton from "../../components/StylishButton";
@@ -5,5 +16,5 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 
 export default function BorrowerExpanded( { unit, onClose } ) {
-
+    
 }
