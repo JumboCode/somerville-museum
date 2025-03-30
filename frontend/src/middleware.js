df5d761abc128328f185cf2d6cec14ec3d107fc5
shaynesidman <147111519+shaynesidman@users.noreply.github.com>
2025-03-30 18:28:27 -0400

    Middleware is all good to go

--- frontend/src/middleware.js
+++ frontend/src/middleware.js
@@ -1,6 +1,13 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isProtectedRoute = createRouteMatcher(['/dashboard(.*)', '/settings(.*)', '/inventory(.*)', '/add(.*)', '/borrowers(.*)', '/edit(.*)'])
+const isProtectedRoute = createRouteMatcher([
+  '/dashboard/:path*',
+  '/settings/:path*', 
+  '/inventory/:path*', 
+  '/add/:path*', 
+  '/borrowers/:path*', 
+  '/edit/:path*',
+])
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) await auth.protect()
