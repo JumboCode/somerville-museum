20c14e79b9bbe83ed23f5d38bd9fd062513830f0
Dan Glorioso <125602370+danglorioso@users.noreply.github.com>
2025-04-13 03:41:02 -0400

    Setting Clerk metadata to true for approved users

--- frontend/api/approve-user.js
+++ frontend/api/approve-user.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
         },
         body: JSON.stringify({
             publicMetadata: {
-                // New users do not have approved permission
+                // Set Clerk metadata to approved
                 approved: true,
             },
         }),
