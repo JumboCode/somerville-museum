6f33d7fb8dee9c508f1602f3ccf4e60acda49893
hkittelberger <holdenkittelberger@gmail.com>
2025-05-14 23:16:46 -0400

    fixed email copy text

--- frontend/api/sendAllEmail.js
+++ frontend/api/sendAllEmail.js
@@ -29,10 +29,12 @@ export default async function handler(req, res) {
                 subject = "Confirmation: Items Borrowed from Somerville Museum";
                 htmlContent = `
                     <h3>Hi there!</h3>
-                    <p>This email confirms you borrowed these items:</p>
+                    <p>This email serves to confirm that the following item(s) have been borrowed from the Somerville Museum:</p>
                     <ul>${items.map(item => `<li>${item}</li>`).join("")}</ul>
                     <p>You will receive another email when the return date approaches.</p>
-                    <p>If you have any questions, please contact <a href="mailto:info@somervillemuseum.org">info@somervillemuseum.org</a>.</p>
+                    <p>If you have any questions, please contact <a href="mailto:info@somervillemuseum.org">info@somervillemuseum.org</a> 
+                       and your email will be directed to the appropriate person. Please return your items in the same conidtion
+                       as when you received them and in the same or a better bag or container.</p>
                 `;
                 break;
 
@@ -51,7 +53,8 @@ export default async function handler(req, res) {
                         <h3>Hi ${borrower_name}!</h3>
                         <p>This email is to remind you that the item(s) listed below are <b>OVERDUE</b>:</p>
                         <ul>${items.map(item => `<li>${item}</li>`).join("")}</ul>
-                        <p>Please reach out to <a href="mailto:info@somervillemuseum.org">info@somervillemuseum.org</a> to arrange a return.</p>
+                        <p>Please reach out to <a href="mailto:info@somervillemuseum.org">info@somervillemuseum.org</a> 
+                           to find an appropriate date and time to return your items, or if you have any questions.</p>
                     `;
                     await sendEmail(recipients, subject, htmlContent);
                 }
@@ -62,7 +65,7 @@ export default async function handler(req, res) {
                 const upcomingItems = await query(`
                     SELECT borrower_name, borrower_email, due_date, json_agg(item_name) AS items
                     FROM borrowed_items 
-                    WHERE due_date BETWEEN CURRENT_DATE AND CURRENT_DATE + INTERVAL '3 days'
+                    WHERE due_date BETWEEN CURRENT_DATE AND CURRENT_DATE + INTERVAL '5 days'
                     GROUP BY borrower_name, borrower_email, due_date
                 `);
 
@@ -71,9 +74,31 @@ export default async function handler(req, res) {
                     subject = "Reminder: Your Borrowed Items Are Due Soon";
                     htmlContent = `
                         <h3>Hi ${borrower_name}!</h3>
-                        <p>This email is to remind you that the following item(s) are due back by <b>${due_date}</b>:</p>
+                        <p>This email is to remind you that the following item(s) to be reutned to the Somerville Museum by <b>${due_date}</b>:</p>
                         <ul>${items.map(item => `<li>${item}</li>`).join("")}</ul>
-                        <p>Please contact <a href="mailto:info@somervillemuseum.org">info@somervillemuseum.org</a> to arrange your return.</p>
+                        <p>Before returning, please review and act on the applicable guidelines below.
+                           Please contact <a href="mailto:info@somervillemuseum.org">info@somervillemuseum.org</a> to arrange your return.</p>
+                        <p><b>Guidelines:</b></p>
+                        <p>
+                            Please return your items in the same or better condition than when you received them. 
+                            If changes in condition have occurred, please let us know. Some issues that arise include 
+                            the loss of a button or hook.  Please try to find these items and keep these with the garment 
+                            when you return it. 
+                        </p>
+                        <p>
+                            White or off-white garments made out of cotton, such as socks, shirts, or women’s shirts 
+                            or chemise, should be cleaned before returning.  These objects can be washed in a washing 
+                            machine on delicate cycle with a fragrance-free detergent and dried in a dryer on low heat. 
+                            Do not overheat or some shrinking may occur. Hang these items on hangers when taking them out 
+                            of the dryer to reduce wrinkling until you have time to bring them back to the Museum. When 
+                            folding them to fit in your bag, try to smooth out the wrinkles.
+                        </p>
+                        <p>
+                            Garment items that are often missed in returns include: socks and accessories such as belts, 
+                            pockets, fichu or neckerchiefs, pins, jewelry, bobby pins and safety pins.  Please return 
+                            everything to avoid us having to track you down.  Thank you!
+                        </p>
+
                     `;
                     await sendEmail(recipients, subject, htmlContent);
                 }
@@ -91,7 +116,7 @@ export default async function handler(req, res) {
                     <h3>Hi ${recipientName}!</h3>
                     <p>This email serves to confirm that the following item(s) have been returned:</p>
                     <ul>${items.map(item => `<li>${item}</li>`).join("")}</ul>
-                    <p>Thank you for your support of the Somerville Museum.</p>
+                    <p>Thank you for volunteering with the Somerville Museum.</p>
                 `;
                 break;
 
